6091b54b3959ff6fab1cb090ad55ae52b0571dcd
Ben Davis <bendavis78@gmail.com>
2016-04-20 10:47:52 -0500

    added authentication functionality

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+gem 'devise'
+
 gem 'materialize-sass'
 
 gem 'httparty'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'welcome#index'
   resources :comments
   resources :blog_posts
+
+  get '/contact-us' => 'welcome#contact_us'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
